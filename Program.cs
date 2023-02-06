ff3f48c94e633cf99cc06e25c05530756898b5c5
Cameron P <cam.peterson@protonmail.com>
2023-02-06 14:44:06 -0600

    Start parsing data.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace Assignment;
 
 class Program {
+    private static string DATA_FILE = "data.txt";
+    
     static void Main(string[] args) {
         // ask for input
         Console.WriteLine("Enter 1 to create data file.");
@@ -9,44 +11,57 @@ class Program {
         // input response
         string? resp = Console.ReadLine();
 
-        if (resp == "1") {
-            // create data file
+        switch (resp) {
+            case "1":
+                // * create data file
+                // ask a question
+                Console.WriteLine("How many weeks of data is needed?");
+                // input the response (convert to int)
+                int weeks = int.Parse(Console.ReadLine() ?? "");
+                // determine start and end date
+                DateTime today = DateTime.Now;
+                // we want full weeks sunday - saturday
+                DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+                // subtract # of weeks from endDate to get startDate
+                DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+                // random number generator
+                Random rnd = new Random();
+                // create file
+                StreamWriter sw = new StreamWriter(DATA_FILE);
 
-            // ask a question
-            Console.WriteLine("How many weeks of data is needed?");
-            // input the response (convert to int)
-            int weeks = int.Parse(Console.ReadLine() ?? "");
-            // determine start and end date
-            DateTime today = DateTime.Now;
-            // we want full weeks sunday - saturday
-            DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
-            // subtract # of weeks from endDate to get startDate
-            DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
-            // random number generator
-            Random rnd = new Random();
-            // create file
-            StreamWriter sw = new StreamWriter("data.txt");
+                // loop for the desired # of weeks
+                while (dataDate < dataEndDate) {
+                    // 7 days in a week
+                    int[] hours = new int[7];
+                    for (int i = 0; i < hours.Length; i++) {
+                        // generate random number of hours slept between 4-12 (inclusive)
+                        hours[i] = rnd.Next(4, 13);
+                    }
 
-            // loop for the desired # of weeks
-            while (dataDate < dataEndDate) {
-                // 7 days in a week
-                int[] hours = new int[7];
-                for (int i = 0; i < hours.Length; i++) {
-                    // generate random number of hours slept between 4-12 (inclusive)
-                    hours[i] = rnd.Next(4, 13);
+                    // M/d/yyyy,#|#|#|#|#|#|#
+                    // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
+                    sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
+                    // add 1 week to date
+                    dataDate = dataDate.AddDays(7);
                 }
 
-                // M/d/yyyy,#|#|#|#|#|#|#
-                // Console.WriteLine($"{dataDate:M/d/yy},{string.Join("|", hours)}");
-                sw.WriteLine($"{dataDate:M/d/yyyy},{string.Join("|", hours)}");
-                // add 1 week to date
-                dataDate = dataDate.AddDays(7);
-            }
+                sw.Close();
+                break;
+            case "2":
+                if (!File.Exists(DATA_FILE)) break;
+                StreamReader file = new StreamReader(
+                    new FileStream("data.txt", FileMode.Open));
 
-            sw.Close();
-        }
-        else if (resp == "2") {
-            // TODO: parse data file
+                while (!file.EndOfStream) {
+                    String curLine = file.ReadLine() ?? "";
+
+                    var rawData = (
+                        dataDate: DateTime.Parse(curLine.Split(',')[0]),
+                        dataSleep: curLine.Split(',')[0].Split('|')
+                    );
+                }
+
+                break;
         }
     }
 }
